b02b177394a161d6f46f780af052ca1873ae1565
lsj <1372573234@qq.com>
2019-07-02 23:13:34 +0800

    小修改

--- src/views/Roles/Roles.vue
+++ src/views/Roles/Roles.vue
@@ -2,8 +2,8 @@
   <div>
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
       <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
     </el-breadcrumb>
 
 
