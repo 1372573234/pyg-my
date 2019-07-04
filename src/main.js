6019cb083709623d716ba3bc384255355b2c558b
lsj <1372573234@qq.com>
2019-07-04 17:47:54 +0800

    添加商品完成

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import "./assets/css/index.css";
 
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "quill/dist/quill.bubble.css";
+
 import axios from "axios";
 
 Vue.use(ElementUI);
